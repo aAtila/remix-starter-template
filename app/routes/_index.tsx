79148c4c011701a5973dc007d3a32ce008213394
Atila Alacan <alacanatila@gmail.com>
2023-11-01 07:57:17 +0100

    index placeholder

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
 	return [
-		{ title: 'New Remix App' },
+		{ title: '🔥' },
 		{ name: 'description', content: 'Welcome to Remix!' },
 	];
 };
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
 	return (
 		<div>
-			<h1 className="text-2xl text-sky-500">My next 🔥 project!</h1>
+			<h1>My next 🔥 project!</h1>
 		</div>
 	);
 }
