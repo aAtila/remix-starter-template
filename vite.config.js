8d07cd4bf91f6ef368fd1f4fba2f2b8c9b44a7b7
Atila Alacan <alacanatila@gmail.com>
2024-01-14 08:46:36 +0100

    add remix-development-tools

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { remixDevTools } from 'remix-development-tools/vite';
 
 export default defineConfig({
 	plugins: [
+		remixDevTools(),
 		remix({
 			ignoredRouteFiles: ['**/.*'],
 			serverModuleFormat: 'esm',
