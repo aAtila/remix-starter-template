cc690a11b072115da2ba70fb7bd89bf793a22b14
Atila Alacan <alacanatila@gmail.com>
2024-04-26 09:03:35 +0200

    add const `COMPANY_NAME`

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,4 +1,5 @@
+export const COMPANY_NAME = 'My Company';
 export const DOMAIN = `domain.com`;
 export const SITE_URL = `https://${DOMAIN}`;
-export const CMS_URL = `https://cms.r3connector.com`;
+export const CMS_URL = `https://cms.domain.com`;
 export const CMS_DATA_MODEL = '';
